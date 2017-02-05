fdc8f0c7b4736d4c77a9400c5a7210ba19632381
Thad <tjaszek@gmail.com>
2017-02-05 17:32:32 -0500

    Game works. Keeps score, plays notes for hit/miss, etc.

--- squaregame.ino
+++ squaregame.ino
@@ -14,9 +14,9 @@
 // Parameters etc.
 #define BUTTON_COUNT 16
 #define NOT_SET -1
-#define EASY_DURATION 400
-#define MEDIUM_DURATION 300
-#define HARD_DURATION 150
+#define EASY_DURATION 700
+#define MEDIUM_DURATION 550
+#define HARD_DURATION 400
 
 // State Machine:
 enum programState {
@@ -32,10 +32,12 @@ Adafruit_Trellis matrix = Adafruit_Trellis();
 Adafruit_TrellisSet trellis =  Adafruit_TrellisSet(&matrix);
 
 short id = NOT_SET;
-int startTime = 0;
-int duration = EASY_DURATION;
+bool chooseNext = true;
+unsigned long startTime = 0;
+unsigned long duration = EASY_DURATION;
 short pointsFor = 0;
 short pointsAgainst = 0;
+unsigned long throttle = 0;
 
 // Sound Effects:
 onenote success_notes[] = {{NOTE_C5, 6}, {NOTE_E5, 12}};
@@ -57,8 +59,15 @@ void initNextGame() {
   }
   trellis.writeDisplay();
 
+  // Reset sounds
+  success.Pause();
+  success.FromTheTop();
+  miss.Pause();
+  miss.FromTheTop();
+
   // No LED is set.
   id = NOT_SET;
+  chooseNext = true;
 
   // TODO get input from user how hard/easy to make the game.
   duration = EASY_DURATION;
@@ -76,22 +85,61 @@ void initNextGame() {
  * playGame handles STATE_PLAY. Handle game play events.
  */
 void playGame() {
-  if (NOT_SET == id) {
-    // Assign an LED to illuminate
-    id = random(BUTTON_COUNT);
-    trellis.setLED(id);
-    trellis.writeDisplay();
+  delay(20);
+
+  if (chooseNext) {
+    short newId = NOT_SET;
+    // Assign an LED to illuminate. Ensure that it's not the same as the last one.
+    do {
+      // Purposefully do more than there are buttons. Want there to be a decent chance
+      // that no LED is turned on, to make it a little less predictable.
+      newId = random(BUTTON_COUNT * 1.5);
+    } while (newId == id);
+    id = newId;
+    chooseNext = false;
+    // If the next one is a button, illuminate it.
+    if (id < BUTTON_COUNT) {
+      trellis.setLED(id);
+      trellis.writeDisplay();
+    }
     startTime = millis();
   } else {
     if (millis() >= (startTime + duration)) {
       trellis.clrLED(id);
       trellis.writeDisplay();
-      id = NOT_SET;
+      chooseNext = true;
+    } else {
+      // readSwitches returns true when a button is pushed or when a button is released.
+      if (trellis.readSwitches()) {
+        // A button was pressed. Which one?
+        if (trellis.justPressed(id)) {
+          // The correct one. Point awarded:
+          Serial.print("+");
+          success.Play();
+          pointsFor++;
+        } else {
+          // Possibly a "just released" event, which we must ignore. So check all buttons
+          // to see if they were pressed. If so, deduct a point. Otherwise, ignore this
+          // event.
+          for (int i=0; i<BUTTON_COUNT; i++) {
+            if (trellis.justPressed(i))
+              {
+                Serial.print("-");
+                miss.Play();
+                pointsAgainst++;
+              }
+          }
+        }
+      }
     }
   }
 
   // Get out of the game by having enough points for or against.
   if ((pointsFor > 10) || (pointsAgainst > 10)) {
+    Serial.print("points for: ");
+    Serial.print(pointsFor);
+    Serial.print(" points against: ");
+    Serial.println(pointsAgainst);
     state = STATE_RESULTS;
   }
 }
@@ -101,7 +149,12 @@ void playGame() {
  * showResults handles STATE_RESULTS. Display the score when the game is over.
  */
 void showResults() {
-  state = STATE_INIT;
+  // Wait for a button push to move on to the next game.
+  if (trellis.readSwitches()) {
+    delay(200);
+    trellis.readSwitches();
+    state = STATE_INIT;
+  }
 }
 
 
@@ -110,6 +163,7 @@ void showResults() {
  * Initialize the random seed; the Trellis board; the melody player; the State.
  */
 void setup() {
+  Serial.begin(9600);
   randomSeed(analogRead(0));
 
   // INT pin requires a pullup
@@ -130,6 +184,7 @@ void setup() {
 void loop() {
   switch (state) {
     case STATE_INIT:
+      Serial.println("call initNextGame()");
       initNextGame();
       break;
 
@@ -138,8 +193,11 @@ void loop() {
       break;
 
     case STATE_RESULTS:
+      Serial.println("call showResults()");
       showResults();
       break;
   }
+  success.Update();
+  miss.Update();
 }
 
